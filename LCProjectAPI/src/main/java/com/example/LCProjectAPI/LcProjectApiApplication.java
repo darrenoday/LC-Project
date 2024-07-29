13d74fcf8d2e0e71f787802ea536edb1af7f281e
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-07-29 12:10:32 -0500

    Created API and UI

---
+++ LCProjectAPI/src/main/java/com/example/LCProjectAPI/LcProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LCProjectAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcProjectApiApplication.class, args);
+	}
+
+}
---
+++ LCProjectAPI/src/test/java/com/example/LCProjectAPI/LcProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LCProjectAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
